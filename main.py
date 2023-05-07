4af288b54859d02349d9d6d8fe2fafb4554b48d6
gabriel <batistutag190@gmail.com>
2023-05-06 22:40:49 -0300

    update main, added more functions in modules

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import baka_tsuki
+import time
+
+category = 'https://www.baka-tsuki.org/project/index.php?title=Category:Light_novel'
 
 if __name__ == '__main__':
-    soup = baka_tsuki.get_soup()
-    list_novels_by_language = baka_tsuki.get_links_light_novels_by_language(soup)
+    start_time = time.time()
+
+    soup = baka_tsuki.get_soup(category)
+    list_novels_by_language = baka_tsuki.get_links_light_novels_by_language(soup=soup)
+    baka_tsuki.get_list_novels(list_novels_by_language=list_novels_by_language)
+    
+    print("--- %s seconds ---" % (time.time() - start_time))
